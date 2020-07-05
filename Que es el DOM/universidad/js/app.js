3a0c1f616fe6ba80c652aefb47b2696e7f62f156
S4NDIX <sandix390@gmail.com>
2020-07-04 21:23:41 -0500

    First commit

---
+++ Que es el DOM/universidad/js/app.js
@@ -0,0 +1,19 @@
+// Eliminar de Local Storage
+localStorage.clear();
+
+
+let elemento;
+
+
+elemento = document;
+
+elemento = elemento.getElementById('lista-cursos')
+
+
+
+elemento.style.backgroundColor = 'BLACK';
+
+
+
+
+
