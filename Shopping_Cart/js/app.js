c38d279fbe5877112e1ea1b4061061fa35ae0a8f
S4NDIX <sandix390@gmail.com>
2020-07-08 12:38:08 -0500

    Added comments

--- Shopping_Cart/js/app.js
+++ Shopping_Cart/js/app.js
@@ -1,3 +1,4 @@
+//Obtener elementos de html
 const shoppingCart = document.getElementById("carrito");
 
 const courses = document.getElementById("lista-cursos");
@@ -6,7 +7,9 @@ const shoppingCartList = document.querySelector("#lista-carrito tbody");
 
 const clearShoppingCart = document.getElementById("vaciar-carrito");
 
+//Agregar los eventsListeners
 eventListeners();
+
 
 function eventListeners() {
   courses.addEventListener("click", courseToAdd);
@@ -18,16 +21,20 @@ function eventListeners() {
   document.addEventListener("DOMContentLoaded", readLocalStorage);
 }
 
+//*Mediante delegation se obtiene el click y si al elemento al que le dio contenia la clase 'agregar-carrito' se obtenia la informacion del curso
 function courseToAdd(e) {
   e.preventDefault();
 
   if (e.target.classList.contains("agregar-carrito")) {
+    //?Se sube dos niveles ya que el boton de agregar esta como hijo del hijo de el contendor de la informacion del curso
     const course = e.target.parentElement.parentElement;
 
     getCourseData(course);
   }
 }
 
+
+//*Se obtiene toda la informacion del curso haciendo queries del elemento a sus respectivas clases, y asignandolas a los atributos del objeto courseData
 function getCourseData(course) {
   const courseData = {
     img: course.querySelector("img").src,
@@ -39,6 +46,7 @@ function getCourseData(course) {
   insertCourse(courseData);
 }
 
+//* Ya con la informacion se crea un table row y que mediante template literals se crea el objeto html que contiene la informacion del curso al que cual se le hizo click y se agrega como un hijo del container del carrito ademas de guardarse en el local storage
 function insertCourse(courseData) {
   const row = document.createElement("tr");
   row.innerHTML = `
@@ -56,6 +64,7 @@ function insertCourse(courseData) {
   saveCourseLocalStorage(courseData);
 }
 
+//* Para remover un curso se detecta el click en el boton de borrar, se sube dos niveles al container del curso y se borra del dom, ademas de guardar su id para posteriormente borrarlo de Local Storage
 function removeCourse(e) {
   e.preventDefault();
 
@@ -69,6 +78,7 @@ function removeCourse(e) {
   removeCourseLocalStorage(courseID);
 }
 
+//* Esta funcion elimina todos los hijos contenidos en el carrito y limpia todo el Local Storage
 function clearCart(e) {
   e.preventDefault();
 
@@ -79,6 +89,7 @@ function clearCart(e) {
   localStorage.clear();
 }
 
+//* Se obtiene la informacion actual de local storage, si habia algo se agrega el nuevo curso y se guarda como string en el Local Storage
 function saveCourseLocalStorage(courseData) {
   let courses = getCoursesLocalStorage();
 
@@ -87,6 +98,7 @@ function saveCourseLocalStorage(courseData) {
   localStorage.setItem("courses", JSON.stringify(courses));
 }
 
+//* Para obtener los cursos se comprueba que no este vacio, de estarlo se creo un array vacio o de lo contrario se obtiene la informacion en formato de string y se convierte a array mediante la libreria de JSON
 function getCoursesLocalStorage() {
   let coursesLS;
 
@@ -99,6 +111,7 @@ function getCoursesLocalStorage() {
   return coursesLS;
 }
 
+//* Para desplegar los cursos al cargar la pagina se obtienen los datos actuales y para cada curso encontrado se crea su elemento html y se hace un append al contendor del carrito
 function readLocalStorage() {
   let coursesLS = getCoursesLocalStorage();
 
@@ -120,6 +133,7 @@ function readLocalStorage() {
   });
 }
 
+//* Para borrar un curso del local storage, se recorre cada curso y si el ID recibido es igual al guardado se remueve del array mediante la funcion splice, despues se guarda el nuevo array como string en el Lolcal Storage
 function removeCourseLocalStorage(courseID) {
   let coursesLS = getCoursesLocalStorage();
 
