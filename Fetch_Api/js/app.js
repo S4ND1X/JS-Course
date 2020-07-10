2ba00b5565fd7e2c2650108e07eecbc967748b08
S4NDIX <sandix390@gmail.com>
2020-07-10 13:12:10 -0500

    Fetch using JSON

--- Fetch_Api/js/app.js
+++ Fetch_Api/js/app.js
@@ -1,3 +1,4 @@
+//*Fetch a un txt
 document.getElementById("txtBtn").addEventListener("click", loadTXT);
 
 function loadTXT() {
@@ -7,6 +8,30 @@ function loadTXT() {
     })
     .then((data) => {
       document.getElementById("resultado").innerHTML = data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+
+//*Fetch a un json
+document.getElementById("jsonBtn").addEventListener("click", loadJSON);
+
+function loadJSON() {
+  fetch("empleados.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let html = ``;
+      data.forEach((element) => {
+        html += `<ul>
+                    <li>${element.nombre}</li>
+                    <li>${element.puesto}</li>
+                </ul>`;
+      });
+      document.getElementById("resultado").innerHTML = html;
     })
     .catch((error) => {
       console.log(error);
