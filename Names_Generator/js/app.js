34c3291acc6e5bdc3b6836cc1ea3c756de9fd6d6
S4NDIX <sandix390@gmail.com>
2020-07-09 21:31:37 -0500

    Request to api and validated inputs

--- Names_Generator/js/app.js
+++ Names_Generator/js/app.js
@@ -1,1 +1,59 @@
+//Formulario
+const generateName = document.getElementById("generar-nombre");
 
+generateName.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const originName = document.getElementById("origen");
+  const selectedOriginName = originName.options[originName.selectedIndex].value;
+  console.log(selectedOriginName);
+
+  const gender = document.getElementById("genero");
+  const selectedGender = gender.options[gender.selectedIndex].value;
+  console.log(selectedGender);
+
+  const cant = document.getElementById("numero").value;
+
+  let url = "";
+  url += "http://uinames.com/api/?";
+
+  if (selectedOriginName !== "") {
+    url += `region=${selectedOriginName}&`;
+  }
+
+  if (selectedGender !== "") {
+    url += `gender=${selectedGender}&`;
+  }
+
+  if (cant) {
+    url += `amount=${cant}&`;
+  }
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", url, true);
+
+  xhr.onload = () => {
+    if (this.status === 200) {
+       const nombres = JSON.parse(this.responseText); 
+      let htmlNombres = `<h2>Nombres Generados</h2>`;
+      htmlNombres+='<ul class="lista">';
+
+      nombres.forEach(nombre => {
+        htmlNombres += `
+
+        <li>${nombre.name}
+        
+        `;
+      });
+
+      htmlNombres+='</ul>';
+
+      document.getElementById('resultado').innerHTML = htmlNombres;
+    }
+  };
+
+  
+
+  xhr.send();
+});
