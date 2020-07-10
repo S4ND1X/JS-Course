09ed7d778fd7b4143dc2b0ea118ab0c7a72f82cf
S4NDIX <sandix390@gmail.com>
2020-07-10 14:09:18 -0500

    Destructing objects

---
+++ ECMAScript6/app.js
@@ -0,0 +1,24 @@
+
+//*Destructing
+
+const client = {
+    name: 'Jorge',
+    type : 'Premium',
+    data : {
+        ubicacion:{
+            city: 'gdl',
+            country: 'mx'
+        },
+        cuenta:{
+            since: '10-12-2020',
+            money: 4000
+        }
+    }
+}
+
+let {data: {ubicacion}} = client;
+console.log(ubicacion);
+
+
+
+
