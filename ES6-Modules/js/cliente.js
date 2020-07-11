809e8acc4e3d0a1e33439c310bef183fe824f2ef
S4NDIX <sandix390@gmail.com>
2020-07-10 19:19:40 -0500

    Importing and exporting functions, variables and classes

--- ES6-Modules/js/cliente.js
+++ ES6-Modules/js/cliente.js
@@ -1,26 +1,21 @@
 //* En este documento se declararon variables y funciones necesarias en otro documento por lo que es necesario que se exporten para que puedan importarse en el otro documento
 
-export const clientName = 'Jorge';
+export const clientName = "Jorge";
 export let savings = 0;
 
-
-export function showInfo(name, saving){
-    return `Cliente: ${name} ahrros: ${saving}`;
+export function showInfo(name, saving) {
+  return `Cliente: ${name} ahrros: ${saving}`;
 }
-
-
 
 //* Exportar clase con metodos
 
+export class Alumno {
+  constructor(name, savings) {
+    this.name = name;
+    this.savings = savings;
+  }
 
-export class Alumno{
-    constructor(name, savings) {
-        this.name = name;
-        this.savings = savings;
-    }
-
-
-    showInfo(){
-        return `El Alumno: ${this.name} tiene $${this.savings} ahorrados.`
-    }
+  showInfo() {
+    return `El Alumno: ${this.name} tiene $${this.savings} ahorrados.`;
+  }
 }
