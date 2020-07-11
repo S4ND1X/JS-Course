ac3d519ed9e739e988f3fafddf3735c9007f9ccc
S4NDIX <sandix390@gmail.com>
2020-07-10 19:19:19 -0500

    Importing and exporting functions, variables and classes

---
+++ ES6-Modules/js/app.js
@@ -0,0 +1,9 @@
+//* Se importan los modulos que vas a utilizar del otro documento, en este caso se importaron dos variables y una funcion, de este modo ya no es necesario vincularlos en index
+import * as clientes from './cliente.js'
+
+
+//* La clase Alumno se declaro en cliente.js pero al ser importada sus metodos y variables pueden ser usadas
+let prueba = new clientes.Alumno(clientes.clientName, clientes.savings);
+
+console.log(prueba.showInfo());
+
---
+++ ES6-Modules/js/cliente.js
@@ -0,0 +1,26 @@
+//* En este documento se declararon variables y funciones necesarias en otro documento por lo que es necesario que se exporten para que puedan importarse en el otro documento
+
+export const clientName = 'Jorge';
+export let savings = 0;
+
+
+export function showInfo(name, saving){
+    return `Cliente: ${name} ahrros: ${saving}`;
+}
+
+
+
+//* Exportar clase con metodos
+
+
+export class Alumno{
+    constructor(name, savings) {
+        this.name = name;
+        this.savings = savings;
+    }
+
+
+    showInfo(){
+        return `El Alumno: ${this.name} tiene $${this.savings} ahorrados.`
+    }
+}
